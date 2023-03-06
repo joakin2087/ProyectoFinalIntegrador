b639824a576c28f8b7f4a89af39171975f5c718d
JoaquinB <joakin_2087@hotmail.com>
2023-03-06 09:19:39 -0300

    alert de salida a red social en JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 
+// Alert que avisa que estas por salir de la pagina hacia otro sitio (redes sociales)
+
+const btnRedes = document.querySelector ("#iconoPie");
+
+btnRedes.addEventListener ("click" , alerta =>{
+    alert("Usted esta por salir de esta pagina");
+});
+
+
