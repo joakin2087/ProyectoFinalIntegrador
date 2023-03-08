ca5bf23ec7ee1565842bec4d1837bf7601222a18
JoaquinB <joakin_2087@hotmail.com>
2023-03-08 09:50:04 -0300

    modal de infodecontacto en javascript

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@ btnRedes.addEventListener ("click" , alerta =>{
     alert("Usted esta por salir de esta pagina");
 });
 
+// Modal para informacion de contacto
 
+const botonModalCentro = document.querySelector("#boton-modal-centro");
+const modalCentro = document.querySelector("#modal-centro");
+const cerrarModalCentro = document.querySelector("#cerrar-modal-centro");
 
+botonModalCentro.addEventListener ("click", (Mostrar) => {
+    modalCentro.classList.add("active");
+}); 
+
+cerrarModalCentro.addEventListener("click", (Cerrar) => {
+    modalCentro.classList.remove("active");
+});
 
